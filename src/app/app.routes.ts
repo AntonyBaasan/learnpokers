3021764061389b9a62df3ac31d3b001e84046ed4
Ankhbayar Baasandorj <abaasandorj@microsoft.com>
2024-08-03 02:39:00 -0400

    add routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { FeaturesPageComponent } from './pages/features-page/features-page.component';
+import { ContactPageComponent } from './pages/contact-page/contact-page.component';
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomePageComponent },
+  { path: 'features', component: FeaturesPageComponent },
+  { path: 'contact', component: ContactPageComponent },
+  { path: '**', component: PageNotFoundComponent }
+];
